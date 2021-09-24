4e533e299b91db8f9cec647f72ee12c794cee798
DreiK-one <Dreikone1@mail.ru>
2021-09-24 15:47:41 +0200

    Add an implementation of the in-memory repository of books

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly List<Book> Books = new List<Book>()
+        {
+            new Book(1, "Art Of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language")
+        };
+        public List<Book> GetByTitle(string titlePart)
+        {
+            return Books.Where(book => book.Title.Contains(titlePart)).ToList();
+        }
+    }
+}
