800f188e2cace0ae4d7770b9f2f3381f4aceb5e2
DreiK-one <Dreikone1@mail.ru>
2021-09-30 13:49:30 +0200

    Stable version

---
+++ domain/Store/Contractors/IPaymentService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Contractors
+{
+    public interface IPaymentService
+    {
+        string Code { get; }
+
+        string Title { get; }
+
+        Form CreateForm(Order order);
+
+        Form MoveNext(int orderId, int step, IReadOnlyDictionary<string, string> values);
+
+        OrderPayment GetPayment(Form form);
+    }
+}
